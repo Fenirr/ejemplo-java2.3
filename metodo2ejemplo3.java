2d69c348a251dc14d3fe340ca89e6b3455cc85c3
Fenirr <162t0305@itsm.edu.mx>
2017-02-18 23:45:59 -0600

    sin retorno ni parametros
    ejemplos de metodos con java

---
+++ metodo2ejemplo3.java
@@ -0,0 +1,14 @@
+package metodo2;
+
+public class ejemplo3 {
+	public static void main(String[]args){
+	
+	    dividir(6,4);
+	}
+		public static void dividir(double v,double w){
+			double dividir= (v / w);
+			System.out.println("el resultado es :"+dividir);
+		}
+		
+		
+	}
